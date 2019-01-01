bba79ffb600a939ae1128d366614a5fa14b9b275
PolyCement <polycement@gmail.com>
2019-01-01 23:07:08 +0000

    improved awesomewm widgets
    also added a very hacky low battery notification

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -194,18 +194,37 @@ local pad_widget = wibox.widget.textbox(" ")
 local div_widget = wibox.widget.textbox(" | ")
 
 -- textclock widget
-local clock_widget = wibox.widget.textclock("%a %d %b, %H:%M")
+local clock_widget = wibox.widget.textclock("🕒 %a %d %b, %H:%M")
 local calendar_popup = calendar.month()
 calendar_popup:attach(clock_widget, "tr")
 
 -- battery monitor
 -- don't add it if there's no battery info
+-- might be best to just make this exclusive to doubleslap branch, idk
 local batdir = io.open("/sys/class/power_supply/BAT0", "r")
 local bat_widget = nil
 if batdir then
     io.close(batdir)
     bat_widget = wibox.widget.textbox()
-    vicious.register(bat_widget, vicious.widgets.bat, "🔋 $2%", 30, "BAT0")
+    -- has the low power warning been shown yet?
+    local low_power_warning_shown = false
+    vicious.register(bat_widget, vicious.widgets.bat, function (widget, args)
+        -- /!\ GOOD PROGRAMMER ALERT /!\
+        -- when power drops below 15%, spawn a notification warning me about it
+        -- todo: move this literally anywhere else cos it sure as hell shouldn't be tacked on here
+        if args[1] == "-" then
+            if args[2] <= 15 and not low_power_warning_shown then
+                low_power_warning_shown = true
+                naughty.notify({ preset = naughty.config.presets.critical,
+                                 title = "Warning! Battery Low!",
+                                 text = args[2] .. "% power remaining" })
+            end
+        else
+            low_power_warning_shown = false
+        end
+        local icon = args[1] == "-" and "🔋" or "🔌"
+        return icon .. " " .. args[2] .. "%"
+    end, 30, "BAT0")
 end
 
 -- volume widget
