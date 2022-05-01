3aba3cfea29830bd9fee971441a0b3ed9cf8da0d
PolyCement <polycement@gmail.com>
2022-05-01 13:26:11 +0100

    awesomewm: fix broken menu

--- awesome/mousebinds/global.lua
+++ awesome/mousebinds/global.lua
@@ -2,6 +2,8 @@
 local gears = require("gears")
 local awful = require("awful")
 
+local menu = require("menu")
+
 global_mousebinds = gears.table.join(
     awful.button({ }, 3, function () menu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
